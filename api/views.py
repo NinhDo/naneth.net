b10f0091d2acae5932935697c1f3cb29e9bd6009
Ninh Do <doquininh@gmail.com>
2018-02-24 23:26:16 +0000

    webhook stuff

--- api/views.py
+++ api/views.py
@@ -1,20 +1,23 @@
+import hmac
+from hashlib import sha1
+
+from decouple import config
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.encoding import force_bytes
+
 import requests
 from ipaddress import ip_address, ip_network
 
 # Create your views here.
-
+@require_POST
 @csrf_exempt
 def hello(request):
     # Verify if request came from GitHub
-    print("hello")
-    print(request)
-    forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
-    print(request.META)
+    forwarded_for = u'{}'.format(request.META.get('REMOTE_ADDR'))
     client_ip_address = ip_address(forwarded_for)
-    print(client_ip_address)
     whitelist = requests.get('https://api.github.com/meta').json()['hooks']
 
     for valid_ip in whitelist:
@@ -23,4 +26,22 @@ def hello(request):
     else:
         return HttpResponseForbidden('Permission denied.')
 
-    return HttpResponse('pong')
+    header_signature = request.META.get('HTTP_X_HUB_SIGNATURE')
+    if header_signature is None:
+        return HttpResponseForbidden('Permission denied.')
+
+    sha_name, signature = header_signature.split('=')
+    if sha_name != 'sha1':
+        return HttpResponseServerError('Operation not supported.', status=501)
+
+    mac = hmac.new(force_bytes(config('GITHUB_WEBHOOK_KEY')), msg=force_bytes(request.body), digestmod=sha1)
+    if not hmac.compare_digest(force_bytes(mac.hexdigest()), force_bytes(signature)):
+        return HttpResponseForbidden('Permission denied.')
+
+    event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
+    if event == 'ping':
+        return HttpResponse('pong')
+    elif event == 'push':
+        return HttpResponse('success')
+
+    return HttpResponse(status=204)
